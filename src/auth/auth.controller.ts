68a7090940a0a9b668d888c66e1e757b1f0f385e
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2022-11-11 15:03:42 -0300

    login working

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Post, Res } from "@nestjs/common";
+import { Body, Controller, Post, Res, UseGuards } from "@nestjs/common";
 import { SingUpDto } from "./dto/sing-up.dto";
 import { UsersService } from "../users/users.service";
 import { SingInDto } from "./dto/sing-in.dto";
 import { AuthService } from "./auth.service";
 import { Response } from "express";
+import { LocalAuthGuard } from "./guard/local-auth.guard";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly userService: UsersService,
               private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post("sing-in")
   async singIn(@Body() singInDto: SingInDto, @Res() res: Response) {
     let token = await this.authService.singIn(singInDto);
