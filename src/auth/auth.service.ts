07ca1f80c51702d8d0bd7b29820db28bfe234e09
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2022-11-11 16:59:50 -0300

    update auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { UsersService } from "../users/users.service";
 import { JwtService } from "@nestjs/jwt";
-import { SingUpDto } from "./dto/sing-up.dto";
-import { SingInDto } from "./dto/sing-in.dto";
 
 @Injectable()
 export class AuthService {
